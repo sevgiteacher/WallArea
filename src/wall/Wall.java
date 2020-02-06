ee2edcd57a38376e566d292531884b004f8122e5
MATH <58793691+sevgiteacher@users.noreply.github.com>
2020-02-06 07:31:48 +0000

    Methods has been implemented

--- src/wall/Wall.java
+++ src/wall/Wall.java
@@ -2,4 +2,48 @@ package wall;
 
 public class Wall {
 
+    private double height=0;
+    private double weight=0;
+	
+    public Wall() {
+    	
+    }
+    
+    public Wall(double height, double weight) {
+		
+		this.height = height;
+		this.weight = weight;
+	}
+
+	public double getHeight() {
+		
+		return height;
+	}
+
+	public void setHeight(double height) {
+		
+		if(height<0)
+		{
+			height=0;
+		}
+		this.height = height;
+	}
+
+	public double getWeight() {
+		
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		
+		if(weight<0)
+		{
+			weight=0;
+		}
+		this.weight = weight;
+	}
+
+   
+  
+
 }
